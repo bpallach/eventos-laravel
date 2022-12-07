089f130b3481b1f9a48f07910fd666f9b6ab68a7
berni696 <bernat696@gmail.com>
2022-12-07 13:26:41 +0100

    Estilos header

--- resources/views/layouts/partials/header.blade.php
+++ resources/views/layouts/partials/header.blade.php
@@ -1,35 +1,34 @@
 <header>
-    <nav class="navbar navbar-expand-lg bg-light">
+    <nav class="navbar navbar-expand-lg bg-danger">
         <div class="container-fluid">
-            <a class="navbar-brand" href="{{ route('home') }}">jSecretarios</a>
+            <a class="navbar-brand link-light" href="{{ route('home') }}">jSecretarios</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
                 aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav">
+                <ul class="navbar-nav text-uppercase">
 
                     @if (Auth::check())
 
                         <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="{{ route('home') }}">Inicio</a>
-                        </li>
-                        
-                        <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="{{ route('profile') }}">Perfil</a>
+                            <a class="nav-link active link-light" aria-current="page" href="{{ route('home') }}">Inicio</a>
                         </li>
 
                         @if (isAdmin())
                             <li class="nav-item">
-                                <a class="nav-link active" aria-current="page" href="{{ route('admin') }}">Admin</a>
+                                <a class="nav-link active link-light" aria-current="page" href="{{ route('admin') }}">Admin</a>
                             </li>
                         @endif
 
                         <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="{{ route('logout') }}">Cerrar sesion</a>
+                            <a class="nav-link active link-light" aria-current="page" href="{{ route('profile') }}">Perfil</a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link active link-light" aria-current="page" href="{{ route('logout') }}">Cerrar sesion</a>
                         </li>
                     @endif                
-                    
                     
                 </ul>
             </div>
